61018964bee0e44088835af7dc27ded8168522c6
euichanhwang <pine6971@naver.com>
2023-08-14 17:39:32 +0900

    example: `동작 파라미터화`로 원하는 동작을 메서드의 인수로 전달할 수 있다.

---
+++ src/main/java/me/euichan/java8/chap02/BehaviorParameterization.java
@@ -0,0 +1,15 @@
+package me.euichan.java8.chap02;
+
+public class BehaviorParameterization {
+
+	/**
+	 * 룸메이트에게
+	 * 1. 우체국에 가서
+	 * 2. 이 고객 번호를 사용하고
+	 * 3. 관리자에게 이야기한 다음에
+	 * 4. 소포를 가져오면 된다.
+	 */
+	public void go(/** 1번부터 4번까지의 동작을 go 메서드의 인수로 전달할 수 있다.**/) {
+		// TODO
+	}
+}
