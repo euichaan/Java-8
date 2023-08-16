cf1994a1d690c3c3380be117b848cc2e06c91276
euichanhwang <pine6971@naver.com>
2023-08-16 09:23:54 +0900

    example: `List.of` example

---
+++ src/main/java/me/euichan/java8/chap03/OfTest.java
@@ -0,0 +1,20 @@
+package me.euichan.java8.chap03;
+
+import java.util.List;
+
+public class OfTest {
+
+	public static void main(String[] args) {
+		List<Integer> integers = List.of(1, 2, 3, 4, 5);
+		System.out.println(integers.getClass().getName()); // java.util.ImmutableCollections$ListN
+
+		// integers.add(6); // UnsupportedOperationException
+		// integers.remove(0); // UnsupportedOperationException
+		// integers.clear(); // UnsupportedOperationException
+		// integers.set(0, 0); // UnsupportedOperationException
+
+		System.out.println(Thread.currentThread().getName());
+		Thread t = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+		t.start();
+	}
+}
